627c524dd3c835ccfb5b1766c4c6339a42e81cee
huangxuan <2257380739@qq.com>
2022-10-16 19:23:38 +0800

    3.0 version

--- src/main/java/vip/huangxuan/Test.java
+++ src/main/java/vip/huangxuan/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         System.out.println("helloWorld2");
+        System.out.println("helloWorld3");
     }
 }
