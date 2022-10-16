1f801bd85be5d773145cc996dbdaacebf206481d
huangxuan <2257380739@qq.com>
2022-10-16 19:23:11 +0800

    2.0 version

--- src/main/java/vip/huangxuan/Test.java
+++ src/main/java/vip/huangxuan/Test.java
@@ -3,5 +3,6 @@ package vip.huangxuan;
 public class Test {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("helloWorld2");
     }
 }
